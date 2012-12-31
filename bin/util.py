9bc1f927eb9b0370b7a915364307392675f2f4b4
Greg Wilson <gvwilson@third-bit.com>
2012-12-30 20:23:41 -0500

    Switching to lxml

--- bin/util.py
+++ bin/util.py
@@ -2,22 +2,7 @@
 Utilities used in Software Carpentry tools.
 """
 
-import re
-try:  # Python 3
-    from io import StringIO
-except:  # Python 2
-    from cStringIO import StringIO
-import xml.etree.ElementTree as ET
-try:  # ElementTree 1.3
-    from xml.etree.ElementTree import ParseError
-except ImportError:  # earlier versions (e.g. with Python 2.6.5)
-    from xml.parsers.expat import ExpatError as ParseError
-try:  # Python 3
-    import html.entities
-    ENTITIES = html.entities.entitydefs
-except ImportError:  # Python 2
-    import htmlentitydefs
-    ENTITIES = htmlentitydefs.entitydefs
+from lxml import etree as ET
 
 #-------------------------------------------------------------------------------
 
@@ -25,23 +10,9 @@ def read_xml(filename, mangle_entities=False):
     """
     Read in a document, returning the ElementTree doc node.
     """
-    try:
-        if mangle_entities:
-            with open(filename, 'r') as reader:
-                data = reader.read()
-                data = data.replace('&', '@@@@')
-                wrapper = StringIO(data)
-                return ET.parse(wrapper)
-        else:
-            with open(filename, 'r') as reader:
-                parser = ET.XMLParser()
-                if hasattr(parser, 'parser'):
-                    parser.parser.UseForeignDTD(True)
-                parser.entity.update(ENTITIES)
-                return ET.parse(reader, parser=parser)
-    except ParseError as e:
-        assert False, \
-               'Unable to parse {0}: {1}'.format(filename, e)
+    parser = ET.HTMLParser()
+    with open(filename, 'r') as reader:
+        return ET.parse(reader, parser)
 
 #-------------------------------------------------------------------------------
 
@@ -49,24 +20,15 @@ def write_xml(filename, doc, unmangle_entities=False):
     """
     Write out a document.
     """
-    def _prettify(match):
-        entity_num = match.group(1)
-        pretty_entity = ORD_TO_ENTITY.get(entity_num)
-        return "&" + (pretty_entity or "#" + entity_num) + ";"
-
-    raw = ET.tostring(doc.getroot())
-    if unmangle_entities:
-        cooked = raw.replace('@@@@', '&')
-    else:
-        cooked = re.sub(r'&#(.*?);', _prettify, raw).replace('&amp;#', '&')
     with open(filename, 'w') as writer:
-        writer.write(cooked)
+        writer.write(ET.tostring(doc.getroot()))
 
 #-------------------------------------------------------------------------------
 
 if __name__ == '__main__':
     import sys
-    if len(sys.argv) == 1:
-        doc = read_xml(sys.argv[1])
-    if len(sys.argv) == 2:
+    if len(sys.argv) not in (2, 3):
+        print >> sys.stderr, 'usage: util.py infile [outfile]'
+    doc = read_xml(sys.argv[1])
+    if len(sys.argv) == 3:
         write_xml(sys.argv[2], doc)
